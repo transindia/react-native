9e7fa5efcbc792f01e949cde9568b367bb210e86
Mahesh Karthu <iiitmahesh@users.noreply.github.com>
2017-04-23 10:08:46 +0530

    bug fix res/drawable
    https://github.com/facebook/react-native/issues/5787
    Error : Original is here. The version qualifier may be implied

--- local-cli/bundle/assetPathUtils.js
+++ local-cli/bundle/assetPathUtils.js
@@ -11,7 +11,7 @@
 function getAndroidAssetSuffix(scale) {
   switch (scale) {
     case 0.75: return 'ldpi';
-    case 1: return 'mdpi';
+    case 1: return 'mdpi-v4';
     case 1.5: return 'hdpi';
     case 2: return 'xhdpi';
     case 3: return 'xxhdpi';
